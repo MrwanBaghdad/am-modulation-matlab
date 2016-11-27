90f826433f1f3953e50c749eb3eb30847b727bcc
MrwanBaghdad <mrwanbaghdad76@gmail.com>
2016-11-27 16:02:02 +0200

    awgn to mod sign and demod the signals

--- test.m
+++ test.m
@@ -30,6 +30,29 @@ carrierF = cos(2*pi*100e3*t);
 
 modsignal=nwav*carrierF;
 
+n0db = awgn(modsignal, 0);
+n10db = awgn(modsignal,10);
+n30db = awgn(modsignal,30);
+
+n0db = resample(n0db,48e3);
+n10db = resample(n10db,48e3);
+n30db = resample (n30db, 48e3);
 
 
+
+plot(t,[n0db,n10db,n30db])
+% [n0db, n10db, n30db] = resample([n0db,n10db,n30db],48e3);
+
+demod0db = n0db .* carrierF
+demod10db = n10db .* carrierF
+demod30db = n30db .* carrierF
+
+demod0db = fftshift(abs(fft(demod0db)))   .* xdFilter
+demod10db = fftshift(abs(fft(demod10db))) .* xdFilter
+demod30db = fftshift(abs(fft(demod30db))) .* xdFilter
+
+
+demod0db = ifft(real(demod0db))
+demod10db = ifft(real(demod10db))
+demod30db = ifft(real(demod30db))
 
