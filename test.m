ec8d23cdbfbe721c6d8dc84422403760e60acfef
MrwanBaghdad <mrwanbaghdad76@gmail.com>
2016-11-27 21:42:24 +0200

    sound of nwav working

--- test.m
+++ test.m
@@ -13,19 +13,19 @@ decPoint = 4e3/48e3;
 fCut = decPoint * l/2;
 iFilter(l/2-fCut : l/2+fCut) = 1;
 
-xdFilter = fftshift(real(fft(iFilter)));
+xdFilter = fftshift(abs(fft(iFilter)));
 
-fwave = iFilter .* spect;
+fwave = iFilter .* spct;
 plot(ft, fwave);
 
-nwav = real(ifft(fwave));
-
+nwav = ifft(ifftshift(fwave));
+nwav = real(nwav);
 plot(ft,nwav);
-sound(nwav);
+sound(nwav,fs);
 
 
-nwav = resample(5*100e3);
-t=linspace(0,length(wav)*1/fs,length(nwav));
+nwav = upsample(nwav, 5*100e3);
+t = linspace(0,length(wav)*1/fs,length(nwav));
 carrierF = cos(2*pi*100e3*t);
 
 modsignal = nwav .*carrierF;
