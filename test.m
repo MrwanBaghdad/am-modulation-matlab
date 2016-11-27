1650938bf386ce5b485881aedc3ab941b290d3cb
MrwanBaghdad <mrwanbaghdad76@gmail.com>
2016-11-27 21:09:28 +0200

    added semicolons no sound

--- test.m
+++ test.m
@@ -3,7 +3,7 @@ wav = audioread('D:/uni/code/analog/eric.wav');
 wav = wav';
 plot(wav);
 ft = linspace(-fs/2,fs/2,length(wav));
-spct = abs(fft(wav));
+spct = (fft(wav));
 spct = fftshift(spct);
 plot(spct);
 
@@ -13,9 +13,9 @@ decPoint = 4e3/48e3;
 fCut = decPoint * l/2;
 iFilter(l/2-fCut : l/2+fCut) = 1;
 
-xdFilter = fftshift(abs(fft(iFilter)));
+xdFilter = fftshift(real(fft(iFilter)));
 
-fwave = xdFilter .* wav;
+fwave = iFilter .* spect;
 plot(ft, fwave);
 
 nwav = real(ifft(fwave));
@@ -28,7 +28,7 @@ nwav = resample(5*100e3);
 t=linspace(0,length(wav)*1/fs,length(nwav));
 carrierF = cos(2*pi*100e3*t);
 
-modsignal=nwav*carrierF;
+modsignal = nwav .*carrierF;
 
 n0db = awgn(modsignal, 0);
 n10db = awgn(modsignal,10);
@@ -43,16 +43,16 @@ n30db = resample (n30db, 48e3);
 plot(t,[n0db,n10db,n30db])
 % [n0db, n10db, n30db] = resample([n0db,n10db,n30db],48e3);
 
-demod0db = n0db .* carrierF
-demod10db = n10db .* carrierF
-demod30db = n30db .* carrierF
+demod0db = n0db .* carrierF;
+demod10db = n10db .* carrierF;
+demod30db = n30db .* carrierF;
 
-demod0db = fftshift(abs(fft(demod0db)))   .* xdFilter
-demod10db = fftshift(abs(fft(demod10db))) .* xdFilter
-demod30db = fftshift(abs(fft(demod30db))) .* xdFilter
+demod0db = fftshift(abs(fft(demod0db)))   .* xdFilter;
+demod10db = fftshift(abs(fft(demod10db))) .* xdFilter;
+demod30db = fftshift(abs(fft(demod30db))) .* xdFilter;
 
 
-demod0db = ifft(real(demod0db))
-demod10db = ifft(real(demod10db))
-demod30db = ifft(real(demod30db))
+demod0db = ifft(real(demod0db));
+demod10db = ifft(real(demod10db));
+demod30db = ifft(real(demod30db));
 
